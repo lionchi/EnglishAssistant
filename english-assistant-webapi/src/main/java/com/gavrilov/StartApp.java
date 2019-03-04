555b6a93ebb660a3028d654fac8b7ee61c9f7195
lionchi <gavrilov.vania@yandex.ru>
2019-03-04 20:25:15 +0300

    Добавил плагины для корректировки сборки проекта

--- english-assistant-webapi/src/main/java/com/gavrilov/StartApp.java
+++ english-assistant-webapi/src/main/java/com/gavrilov/StartApp.java
@@ -1,15 +1,24 @@
 package com.gavrilov;
 
-import org.gavrilov.configuration.CoreConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@Import({CoreConfiguration.class})
+@EntityScan(basePackages = "org.gavrilov.domain")
+@EnableJpaRepositories(basePackages = "org.gavrilov.repository")
+@ComponentScan({"org.gavrilov", "com.gavrilov"})
 public class StartApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(StartApp.class);
     }
 }
